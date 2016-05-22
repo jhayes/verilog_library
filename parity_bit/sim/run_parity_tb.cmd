8797be1b58bf2073b245e599e931f945844c7d16
jhayes <jhayes@users.noreply.github.com>
2016-05-22 16:58:18 +0100

    Changing run script to give informative name for intermediate outputs.

--- parity_bit/sim/run_parity_tb.cmd
+++ parity_bit/sim/run_parity_tb.cmd
@@ -1,3 +1,3 @@
-iverilog -g2012 ..\tb\parity_tb.v ..\tb\shift_reg.v ..\rtl\parity_bit_gen.v 
-vvp a.out
+iverilog -o compiled_files.out -g2012 ..\tb\parity_tb.v ..\tb\shift_reg.v ..\rtl\parity_bit_gen.v 
+vvp compiled_files.out
 gtkwave dump.vcd

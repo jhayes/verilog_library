5189219b5ad4aad2d9e953d4362379760a44deb5
jhayes <jhayes@users.noreply.github.com>
2016-05-22 20:57:02 +0100

    Set up tasks. Changed test to run through loop of random values.
    Setup an init() task for initialization of clk, rst.
    Setup a re_init() task for re-init of values for when restarting test.
    Setup a reset_pulse_gen() task, which when called generates a reset
    pulse.
    Changed test so that data_words are randomly generated and passed into
    the data_gen module at runtime. This allows for the test to rerun
    multiple times now.
    Set up an overall failure flag that is set if any of the tests fails.

--- parity_bit/tb/parity_tb.v
+++ parity_bit/tb/parity_tb.v
@@ -16,37 +16,55 @@ module parity_tb();
 	 parameter SCLK_PERIOD = 20;
 	 parameter START_PATTERN = 4'b1001; // Data word to check for parity.
 	 parameter NUM_BITS = 8'd4; // Number of bits in data word.
-	 parameter EVEN_PARITY_BIT = 1'b0; // If set, DUT will indicate even parity.
+	 parameter EVEN_PARITY_BIT = 1'b1; // If set, DUT will indicate even parity.
+	 parameter NUM_TESTS = 5; // Number of times to restart simulation and rerun test.
 	 
 	 reg sclk;
 	 reg rst_n;
+	 reg start_pulse;
 	 
 	 wire data_in;
 	 wire wr_en;
 	 wire parity_bit;
 	 reg 	tb_answer;
+	 reg 	test_failed;
+	 
+   reg [NUM_BITS-1:0] data_word;
+   
+   integer 						test_cnt;
 	 
 	 always begin // clk_gen
 			#(SCLK_PERIOD/2) sclk = ~sclk;
 	 end
 	 
-	 initial begin // rst_gen
-      sclk = 1'b0;  // Initialize clock.
-			rst_n = 1'b1;
-		 	#100;
-			rst_n = 1'b0;
-			#100;
-			rst_n = 1'b1;
-			#1000;
-      tb_answer = (^START_PATTERN) ^ (EVEN_PARITY_BIT);   // Taking our START_PATTERN and XORing it, amounts to an odd checksum. XORing with EVEN_PARITY will invert answer if required.
+	 initial begin // test
+			init();
+			for(test_cnt=0; test_cnt < NUM_TESTS; test_cnt=test_cnt+1) begin
+         re_init();
+         @(negedge wr_en); // Wait for wr_en to go low (indicates transmission is finished).
+				 @(posedge sclk);
+				 
+         tb_answer = (^data_word) ^ (EVEN_PARITY_BIT);   // Taking our START_PATTERN and XORing it, amounts to an odd checksum. XORing with EVEN_PARITY will invert answer if required.
+         
+         $display("============== Test %0d ==============", test_cnt);
+         $display("============== Settings ==============");
+         $display("Given data %0b, and EVEN_PARITY_BIT = %0b", START_PATTERN, EVEN_PARITY_BIT);
+         $display("parity_bit_gen calculated %0b, and testbench calculated %0b.", parity_bit, tb_answer);
+         
+         $display("=============== Answer ===============");
+         if(tb_answer == parity_bit) $display("Parity bit calculation successful.");
+         else begin
+						$display("Parity bit calculation failed.");
+						test_failed = 1'b1;
+				 end
+				 $display("\n\n");
+      end
 
-      $display("============== Settings ==============");
-      $display("Given data %0b, and EVEN_PARITY_BIT = %0b", START_PATTERN, EVEN_PARITY_BIT);
-      $display("parity_bit_gen calculated %0b, and testbench calculated %0b.", parity_bit, tb_answer);
-      
-      $display("=============== Answer ===============");
-      if(tb_answer == parity_bit) $display("Parity bit calculation successful.");
-      else $display("Parity bit calculation failed.");
+			$display("\n");
+			$display("============= Final Check =============");
+			if(test_failed) $display("Overall Test Failure.");
+			else $display("All tests passed.");
+      $display("======================================");
 			$finish;
 	 end
 
@@ -54,7 +72,7 @@ module parity_tb();
       $dumpfile("dump.vcd");
       $dumpvars(0);
 	 end
-	 
+   
    parity_bit_gen 
 		 #(
 			 .EVEN_PARITY_BIT(EVEN_PARITY_BIT)
@@ -69,14 +87,43 @@ module parity_tb();
 
 	 shift_reg 
 		 #(
-			 .START_PATTERN(START_PATTERN), 
 			 .NUM_BITS(NUM_BITS)
 			 )
 	 data_gen (
 						 .clk(sclk),
 						 .rst_n(rst_n),
 						 .wr_en(wr_en),
+             .start_word(data_word),
 						 .data_out(data_in)
 						 );
+
+
+	 task init(); // Init of clk, reset and test_failed flag on startup.
+			begin
+				 test_failed = 1'b0;
+				 sclk = 1'b0;
+				 rst_n = 1'b1;
+			end
+	 endtask
+
+   task re_init(); // Re-initialize reset on restart of test and set up a new data_word for transmission.
+			begin
+         data_word = $random;
+         rst_n = 1'b1;
+				 gen_reset_pulse();
+			end
+   endtask
+
+   
+   task gen_reset_pulse(); // Generates a reset pulse.
+			begin
+				 rst_n = 1'b1;
+				 repeat(3) @(posedge sclk);
+				 rst_n = 1'b0;
+				 repeat(5) @(posedge sclk);
+				 #(SCLK_PERIOD/4); // Offset the release of the reset from clk for ease in scanning waveforms.
+				 rst_n = 1'b1;
+			end
+   endtask
    
 endmodule

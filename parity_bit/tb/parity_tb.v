8186812659ce49e325ad335ca03d1c768a92bd69
jhayes <jhayes@users.noreply.github.com>
2016-05-22 18:40:02 +0100

    Added a check in the testbench on the result.
    We XOR the START_PATTERN in the testbench as a check. This is then XORed
    with EVEN_PARITY_BIT to invert the answer if we're set up for outputting
    an even parity bit.
    
    Essentially we verify that serialization of the data word is working.
    
    Adding of debug statements in indicate answer.

--- parity_bit/tb/parity_tb.v
+++ parity_bit/tb/parity_tb.v
@@ -3,6 +3,9 @@
 // out a fixed pattern from a shift register along
 // with wr_en pulses.
 //
+// Checks output answer at the end and indicates 
+// success or failure.
+//
 // Depending on setting of EVEN_PARITY_BIT, output
 // wire 'parity_bit' will indicate result.
 
@@ -11,9 +14,9 @@
 module parity_tb();
 
 	 parameter SCLK_PERIOD = 20;
-	 parameter START_PATTERN = 4'b1011; // Data word to check for parity.
+	 parameter START_PATTERN = 4'b1001; // Data word to check for parity.
 	 parameter NUM_BITS = 8'd4; // Number of bits in data word.
-	 parameter EVEN_PARITY_BIT = 1'b1; // If set, DUT will indicate even parity.
+	 parameter EVEN_PARITY_BIT = 1'b0; // If set, DUT will indicate even parity.
 	 
 	 reg sclk;
 	 reg rst_n;
@@ -21,19 +24,29 @@ module parity_tb();
 	 wire data_in;
 	 wire wr_en;
 	 wire parity_bit;
+	 reg 	tb_answer;
 	 
 	 always begin // clk_gen
 			#(SCLK_PERIOD/2) sclk = ~sclk;
 	 end
 	 
 	 initial begin // rst_gen
+      sclk = 1'b0;  // Initialize clock.
 			rst_n = 1'b1;
-			sclk = 1'b0;
-			#100;
+		 	#100;
 			rst_n = 1'b0;
 			#100;
 			rst_n = 1'b1;
 			#1000;
+      tb_answer = (^START_PATTERN) ^ (EVEN_PARITY_BIT);   // Taking our START_PATTERN and XORing it, amounts to an odd checksum. XORing with EVEN_PARITY will invert answer if required.
+
+      $display("============== Settings ==============");
+      $display("Given data %0b, and EVEN_PARITY_BIT = %0b", START_PATTERN, EVEN_PARITY_BIT);
+      $display("parity_bit_gen calculated %0b, and testbench calculated %0b.", parity_bit, tb_answer);
+      
+      $display("=============== Answer ===============");
+      if(tb_answer == parity_bit) $display("Parity bit calculation successful.");
+      else $display("Parity bit calculation failed.");
 			$finish;
 	 end
 
